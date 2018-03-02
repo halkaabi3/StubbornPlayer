3fd52f2904271ce5b582048374736c551576d876
halkaabi3 <halkaabi3@mail.csuchico.edu>
2018-03-01 20:02:33 -0800

    game

--- main.cpp
+++ main.cpp
@@ -6,6 +6,31 @@ using namespace std;
 int main()
 {
   SecretDoor game;
-    
+
+int num;
+int win;
+
+cout<<"How many rounds do you want to play?"<<endl;
+
+cin>>num;
+
+for(int index=0; index<num; index++){
+
+game.newGame();
+game.guessDoorC();
+game.guessDoorC();
+
+if(game.isWinner()==true)
+{
+win++;
+}}
+
+cout<<"You have won"<<win<<endl;
+
+
+
+
+
+
   return 0;
 }
